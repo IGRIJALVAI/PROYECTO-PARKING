9f6f49cc5429df410f73c431937cdcf7b9bd5122
grija <grija@SOMBRA>
2025-10-22 20:09:43 -0600

    Proyecto 1.2

---
+++ src/main/java/com/mycompany/proyecto_estacionamiento/Areas.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_estacionamiento;
+
+/**
+ *
+ * @author grija
+ */
+public class Areas {
+    
+    String Id ;
+    String nombreA ;
+    String capacidad ;
+    String TipoVehiculo ;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getNombreA() {
+        return nombreA;
+    }
+
+    public void setNombreA(String nombreA) {
+        this.nombreA = nombreA;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipoVehiculo() {
+        return TipoVehiculo;
+    }
+
+    public void setTipoVehiculo(String TipoVehiculo) {
+        this.TipoVehiculo = TipoVehiculo;
+    }
+    
+    
+}
